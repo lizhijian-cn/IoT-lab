98af729e82345922c70a93a1245c7763c6f50c4c
Li Zhijian <zjli2000@gmail.com>
2021-11-12 20:58:09 +0800

    clear

--- app/src/main/java/it/moondroid/coverflowdemo/CoverFlowActivity.java
+++ app/src/main/java/it/moondroid/coverflowdemo/CoverFlowActivity.java
@@ -32,89 +32,40 @@ public class CoverFlowActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_coverflow);
-
-        mData.add(new GameEntity(R.drawable.image_1, R.string.title1));
-        mData.add(new GameEntity(R.drawable.image_2, R.string.title2));
-        mData.add(new GameEntity(R.drawable.image_3, R.string.title3));
-        mData.add(new GameEntity(R.drawable.image_4, R.string.title4));
-        mData.add(new GameEntity(R.drawable.image_5,R.string.title5));
+//
+//        setContentView(R.layout.activity_coverflow);
+//
+//        mData.add(new GameEntity(R.drawable.image_1, R.string.title1));
+//        mData.add(new GameEntity(R.drawable.image_2, R.string.title2));
+//        mData.add(new GameEntity(R.drawable.image_3, R.string.title3));
+//        mData.add(new GameEntity(R.drawable.image_4, R.string.title4));
+//        mData.add(new GameEntity(R.drawable.image_5,R.string.title5));
 //        mData.add(new GameEntity(R.drawable.image_5,R.string.title5));
 //        mData.add(new GameEntity(R.drawable.image_6,R.string.title6));
 
-        mTitle = (TextSwitcher) findViewById(R.id.title);
-        mTitle.setFactory(new ViewSwitcher.ViewFactory() {
-            @Override
-            public View makeView() {
-                LayoutInflater inflater = LayoutInflater.from(CoverFlowActivity.this);
-                TextView textView = (TextView) inflater.inflate(R.layout.item_title, null);
-                textView.setTextSize(40);
-                return textView;
-            }
-        });
-        Animation in = AnimationUtils.loadAnimation(this, R.anim.slide_in_top);
-        Animation out = AnimationUtils.loadAnimation(this, R.anim.slide_out_bottom);
-        mTitle.setInAnimation(in);
-        mTitle.setOutAnimation(out);
+//        mTitle = (TextSwitcher) findViewById(R.id.title);
+//        mTitle.setFactory(new ViewSwitcher.ViewFactory() {
+//            @Override
+//            public View makeView() {
+//                LayoutInflater inflater = LayoutInflater.from(CoverFlowActivity.this);
+//                TextView textView = (TextView) inflater.inflate(R.layout.item_title, null);
+//                textView.setTextSize(40);
+//                return textView;
+//            }
+//        });
+//        Animation in = AnimationUtils.loadAnimation(this, R.anim.slide_in_top);
+//        Animation out = AnimationUtils.loadAnimation(this, R.anim.slide_out_bottom);
+//        mTitle.setInAnimation(in);
+//        mTitle.setOutAnimation(out);
+//
+//        mAdapter = new CoverFlowAdapter(this);
+//        mAdapter.setData(mData);
+//        mCoverFlow = (FeatureCoverFlow) findViewById(R.id.coverflow);
+//        mCoverFlow.setAdapter(mAdapter);
 
-        mAdapter = new CoverFlowAdapter(this);
-        mAdapter.setData(mData);
-        mCoverFlow = (FeatureCoverFlow) findViewById(R.id.coverflow);
-        mCoverFlow.setAdapter(mAdapter);
+        Intent intent2 = new Intent("com.topelec.buscard.ACTION_BUSCARD");
+        startActivity(intent2);
 
-        mCoverFlow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                while (position > mData.size() -1) {
-                    position = position - mData.size();
-                }
-
-                Toast.makeText(CoverFlowActivity.this,
-                        getResources().getString(mData.get(position).titleResId),
-                        Toast.LENGTH_SHORT).show();
-
-                switch (position) {
-                    case 0:
-                        Intent intent = new Intent("com.topelec.smarthome.ACTION_SMARTHOME");
-                        startActivity(intent);
-                        break;
-                    case 1:
-                        Intent intent1 = new Intent("com.topelec.smartagriculture.ACTION_AGRICULTURE");
-                        startActivity(intent1);
-                        break;
-                    case 2:
-                        Intent intent2 = new Intent("com.topelec.buscard.ACTION_BUSCARD");
-                        startActivity(intent2);
-                        break;
-                    case 3:
-                        Intent intent3 = new Intent("com.topelec.foodtraceability.ACTION_FOODTRACEABILITY");
-                        startActivity(intent3);
-                        break;
-                    case 4:
-                        Intent intent4 = new Intent("com.topelec.sensortest.ACTION_ZIGBEE");
-                        startActivity(intent4);
-                        break;
-                    case 5:
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
-
-        mCoverFlow.setOnScrollPositionListener(new FeatureCoverFlow.OnScrollPositionListener() {
-            @Override
-            public void onScrolledToPosition(int position) {
-                mTitle.setText(getResources().getString(mData.get(position).titleResId));
-
-            }
-
-            @Override
-            public void onScrolling() {
-                mTitle.setText("");
-            }
-        });
 
     }
 
